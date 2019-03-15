49cc8113107105a6d71f7a5fde046e8bc54fc6d4
Yadav <vyadav44@ms.ds.uhc.com>
2019-03-15 20:48:15 +0530

    Python String Basics

---
+++ PrintText.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 15 20:13:50 2019
+
+@author: vyadav44 
+"""
+
+#Print with tabs and new lines
+print ("This is the line of string you want me to print!!!")
+
+print(6*7)
+
+print("Number 1\tThe Larch\nNumber 2\tThe Horse Chestnut")
+
+
+#Get user Input
+
+name=input("Enter your name: ")
+
+print("Good day "+ name)
+
